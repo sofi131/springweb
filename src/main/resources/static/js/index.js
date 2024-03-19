1cd9ab9be346125c0c0db62ba085f2d88b21d93f
sofi131 <sofiasanmartin131@gmail.com>
2024-03-19 20:22:06 +0100

    Poniendo el js.

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,11 +1,15 @@
 window.onload = () => {
     document.getElementById("btnPedirDatos").onclick = () => {
-        let url = location.href + "api/pedirdatos";
+
+        let url = location.origin + "/api/pedirdatos?dat=34&texto=hola";
         fetch(url)
             .then(datos => datos.json())
             .then(datos => {
+                let lista=document.getElementById("lista");
                 for (let i = 0; i < datos.length; i++) {
-                    console.log(datos[i]);
+                    let liElement=document.createElement("li");
+                    liElement.textContent=datos[i];
+                    lista.append(liElement);
                 }
             })
     }
