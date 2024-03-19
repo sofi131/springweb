d443476d26a03934cd66772f489d18f792e900a4
sofi131 <sofiasanmartin131@gmail.com>
2024-03-19 17:02:32 +0100

    ind3ex.html y ViewController

--- src/main/java/com/ceica/springweb/ViewController.java
+++ src/main/java/com/ceica/springweb/ViewController.java
@@ -1,12 +1,15 @@
 package com.ceica.springweb;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller //así le decimos que es un controller (html) si fuera rest datos(string cadena de texto)
 public class ViewController {
-    @GetMapping("/index")
-    public String index() {
+    @GetMapping("/")
+    public String index(Model model) {
+        String nombre="Tomás";
+        model.addAttribute("name", nombre);
         return "index";
     }
 }
